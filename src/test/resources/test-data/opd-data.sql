6a1000ff7ead354566387a7c76d03bdfb94041a8
Affandi Pratama <afandip911@gmail.com>
2025-08-08 20:37:40 +0700

    Setup test dan data untuk test database menggunakan database in memory H2

---
+++ src/test/resources/test-data/opd-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO opd (id, kode_opd, nama_opd, created_date, last_modified_date) VALUES
+(1, 'OPD-001', 'Badan Perencanaan Pembangunan Daerah', NOW(), NOW()),
+(2, 'OPD-002', 'Dinas Pendidikan', NOW(), NOW()),
+(3, 'OPD-003', 'Dinas Kesehatan', NOW(), NOW());
