be9a41e5e39a6bfa00f16332f1be32bd0640e6d4
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2026-03-12 22:51:35 +0700

    kembalikan lagi latest jabatan

--- src/main/java/cc/kertaskerja/tppkepegawaian/jabatan/domain/JabatanService.java
+++ src/main/java/cc/kertaskerja/tppkepegawaian/jabatan/domain/JabatanService.java
@@ -176,7 +176,7 @@ public class JabatanService {
         List<Jabatan> jabatans = jabatanRepository.findByKodeOpd(kodeOpd);
 
         // ambil jabatan terbaru per nip
-        Map<String, Jabatan> latestJabatanPerNip = PeriodeUtils.latestPerKeyFlexible(
+        Map<String, Jabatan> latestJabatanPerNip = PeriodeUtils.latestPerKeyUntil(
                 jabatans,
                 bulan,
                 tahun,
